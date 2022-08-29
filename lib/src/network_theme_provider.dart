308338b07afbc55f6f667aa694544cbabd32a12a
sarbagyastha <sarbagyastha@gmail.com>
2022-08-29 15:50:57 +0545

    :art: added `fallbackAssetPath` to network theme provider

--- lib/src/network_theme_provider.dart
+++ lib/src/network_theme_provider.dart
@@ -8,6 +8,7 @@ class NetworkThemeProvider<T extends Object> extends AcmeThemeProvider<T> {
     super.key,
     required String url,
     required super.builder,
+    this.fallbackAssetPath,
     this.headers,
     super.overrideFn,
     super.customColorsConverterCreator,
@@ -16,12 +17,18 @@ class NetworkThemeProvider<T extends Object> extends AcmeThemeProvider<T> {
     assert(_uri != null, 'Invalid Theme URL: $url');
   }
 
+  /// The fallback asset theme path.
+  final String? fallbackAssetPath;
+
   final Map<String, String>? headers;
 
   late final Uri? _uri;
 
   @override
   Widget build(BuildContext context) {
+    final hasFallback =
+        fallbackAssetPath != null && fallbackAssetPath!.isNotEmpty;
+
     return FutureBuilder<Response>(
       future: get(_uri!, headers: headers),
       builder: (context, snapshot) {
@@ -29,6 +36,13 @@ class NetworkThemeProvider<T extends Object> extends AcmeThemeProvider<T> {
         if (snapshot.hasData) {
           theme = AcmeTheme<T>.fromJson(
             snapshot.data!.body,
+            customColorsConverterCreator: customColorsConverterCreator,
+          );
+        } else if (hasFallback) {
+          return AcmeThemeProvider.asset(
+            path: fallbackAssetPath!,
+            builder: builder,
+            overrideFn: overrideFn,
             customColorsConverterCreator: customColorsConverterCreator,
           );
         } else {
