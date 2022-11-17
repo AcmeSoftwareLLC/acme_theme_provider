b4b950a5c0bd26c3135bf56e523dff03b8b8b07a
sarbagyastha <sarbagyastha@gmail.com>
2022-11-17 18:51:53 +0545

    :art: break down main.dart

--- playground/lib/main.dart
+++ playground/lib/main.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 
 import 'package:acme_theme_provider/acme_theme_provider.dart';
 import 'package:flutter/material.dart';
-
-import 'components/one_app_bar.dart';
-import 'components/two_app_bar.dart';
+import 'package:playground/playground_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -42,43 +40,9 @@ class MyApp extends StatelessWidget {
           title: 'Playground',
           theme: theme.lightTheme,
           darkTheme: theme.darkTheme,
-          home: const MyHomePage(),
+          home: const PlaygroundPage(),
         );
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Playground'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            OneAppBar(
-              title: Text('One App Bar'),
-              leading: Icon(Icons.menu),
-            ),
-            TwoAppBar(
-              title: Text('Two App Bar'),
-              leading: Icon(Icons.menu),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ playground/lib/playground_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'components/one_app_bar.dart';
+import 'components/two_app_bar.dart';
+
+class PlaygroundPage extends StatelessWidget {
+  const PlaygroundPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Playground'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            OneAppBar(
+              title: Text('One App Bar'),
+              leading: Icon(Icons.menu),
+            ),
+            TwoAppBar(
+              title: Text('Two App Bar'),
+              leading: Icon(Icons.menu),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
