1a792b8dc29133461fc4d8b04c994c5ab8fd183f
SarvagyaRamBhandari <sarvagya.bhandari@acme-software.com>
2022-11-25 16:40:17 +0545

    new ui for add note

--- example/lib/features/note/presentation/note_ui.dart
+++ example/lib/features/note/presentation/note_ui.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:clean_framework/clean_framework_providers.dart';
 import 'package:clean_framework_router/clean_framework_router.dart';
 import 'package:example/features/note/presentation/note_presenter.dart';
@@ -28,6 +27,47 @@ class NoteUI extends UI<NoteViewModel> {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        isExtended: true,
+        onPressed: () {
+          if (viewModel.title.isEmpty && viewModel.content.isEmpty) {
+            showErrorSnackBar(
+              context,
+              'Both the title and description are empty...',
+            );
+          } else if (viewModel.title.isEmpty) {
+            showErrorSnackBar(
+              context,
+              'The title text is empty',
+            );
+          } else if (viewModel.content.isEmpty) {
+            showErrorSnackBar(
+              context,
+              'The content text is empty',
+            );
+          } else if (viewModel.imagePath.isEmpty) {
+            showErrorSnackBar(
+              context,
+              'Please upload image',
+            );
+          } else {
+            showOKDialog(
+                context: context,
+                title: 'Are you sure you wanna add to the notes?',
+                content: 'This will add to your note',
+                onOk: () {
+                  viewModel.addNote();
+                  context.router.push(Routes.home);
+                  viewModel.refresh();
+                });
+          }
+        },
+        icon: Icon(Icons.add),
+        label: Text(
+          'Add note',
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
+      ),
       body: NestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[
@@ -35,7 +75,7 @@ class NoteUI extends UI<NoteViewModel> {
               floating: false,
               pinned: true,
               automaticallyImplyLeading: false,
-              expandedHeight: 200,
+              expandedHeight: screenHeight / 3,
               backgroundColor: Theme.of(context).primaryColor,
               flexibleSpace: FlexibleSpaceBar(
                 centerTitle: true,
@@ -59,8 +99,6 @@ class NoteUI extends UI<NoteViewModel> {
                     ? const SizedBox()
                     : Image.file(
                         File(viewModel.imagePath),
-                        height: 300,
-                        width: 500,
                         fit: BoxFit.fill,
                       ),
               ),
@@ -92,185 +130,33 @@ class AddNoteUIBody extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
-          Text('Title'),
-          SizedBox(
-            height: 5,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(),
+          TextField(
+            style: Theme.of(context).textTheme.titleLarge,
+            cursorColor: Theme.of(context).primaryColor,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              focusColor: Theme.of(context).primaryColor,
+              hintText: 'Note title',
             ),
-            child: TextField(
-              decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(8),
-                  ),
-                ),
-              ),
-              onChanged: (val) => viewModel.enterTitle(val),
-            ),
+            onChanged: (val) => viewModel.enterTitle(val),
           ),
           SizedBox(
             height: 10,
           ),
-          Text('Description'),
-          SizedBox(
-            height: 5,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(),
-            ),
-            child: TextField(
-              maxLines: 10,
-              decoration: InputDecoration(
-                hintText: 'Type something.....',
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(8),
-                  ),
-                ),
-              ),
-              onChanged: (val) => viewModel.enterContent(val),
-            ),
+          TextField(
+            style: Theme.of(context).textTheme.labelMedium,
+            cursorColor: Theme.of(context).primaryColor,
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                focusColor: Theme.of(context).primaryColor,
+                hintText: 'Note description'),
+            maxLines: 30,
+            onChanged: (val) => viewModel.enterContent(val),
           ),
           SizedBox(
             height: 10,
-          ),
-          // Center(
-          //   child: GestureDetector(
-          //     onTap: () => viewModel.openGallery,
-          //     child: Container(
-          //       height: 50,
-          //       width: 100,
-          //       decoration: BoxDecoration(
-          //         color: Theme.of(context).colorScheme.primaryContainer,
-          //         borderRadius: BorderRadius.circular(12),
-          //       ),
-          //       child: Center(
-          //         child: Text(
-          //           'Add image',
-          //           style: Theme.of(context).textTheme.labelMedium,
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          // SizedBox(
-          //   height: 10,
-          // ),
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                if (viewModel.title.isEmpty && viewModel.content.isEmpty) {
-                  showErrorSnackBar(
-                    context,
-                    'Both the title and description are empty...',
-                  );
-                } else if (viewModel.title.isEmpty) {
-                  showErrorSnackBar(
-                    context,
-                    'The title text is empty',
-                  );
-                } else if (viewModel.content.isEmpty) {
-                  showErrorSnackBar(
-                    context,
-                    'The content text is empty',
-                  );
-                } else {
-                  showOKDialog(
-                      context: context,
-                      title: 'Are you sure you wanna add to the notes?',
-                      content: 'This will add to your note',
-                      onOk: () {
-                        viewModel.addNote();
-                        context.router.push(Routes.home);
-                        viewModel.refresh();
-                      });
-                }
-              },
-              child: Container(
-                height: 50,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: Text(
-                    'Add note',
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ),
-              ),
-            ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class AddNoteButton extends StatelessWidget {
-  AddNoteButton({
-    required this.titleText,
-    required this.contentText,
-    required this.viewModel,
-  });
-
-  final NoteViewModel viewModel;
-  final String titleText;
-  final String contentText;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print(titleText);
-        print(contentText);
-        if (titleText.isEmpty && contentText.isEmpty) {
-          showErrorSnackBar(
-            context,
-            'Both the title and description are empty...',
-          );
-        } else if (titleText.isEmpty) {
-          showErrorSnackBar(
-            context,
-            'The title text is empty',
-          );
-        } else if (contentText.isEmpty) {
-          showErrorSnackBar(
-            context,
-            'The content text is empty',
-          );
-        } else {
-          showOKDialog(
-              context: context,
-              title: 'Are you sure you wanna add to the notes?',
-              content: 'This will add to your note',
-              onOk: () {
-                viewModel.enterTitle(titleText);
-                viewModel.enterContent(contentText);
-                viewModel.addNote();
-              });
-        }
-      },
-      child: Container(
-        height: 50,
-        width: 100,
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primaryContainer,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Center(
-          child: Text(
-            'Add note',
-            style: Theme.of(context).textTheme.labelMedium,
-          ),
-        ),
       ),
     );
   }
